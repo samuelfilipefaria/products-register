77df5317063ab21277e9f992a07d2101088e2616
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-03-15 09:52:50 -0300

    Creating the form (also translating the text to Portuguese)

---
+++ script.js
@@ -0,0 +1,7 @@
+function openModal() {
+  document.getElementById("registerModal").classList.add("is-active")
+}
+
+function closeModal() {
+  document.getElementById("registerModal").classList.remove("is-active");
+}
