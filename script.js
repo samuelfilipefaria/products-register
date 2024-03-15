9ed86fc5ec65f2c601b8ea0c76b0143f0ed0c23e
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-03-15 10:46:59 -0300

    Implementing product registration and listing

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+products = []
+
+function addProduct() {
+  products.push({
+    name: document.getElementById("nameInput").value,
+    description: document.getElementById("descriptionInput").value,
+    value: document.getElementById("valueInput").value,
+    availability: document.getElementById("availabilityInput").value,
+  });
+
+  closeModal();
+  cleanInputs();
+  loadProducts();
+}
+
+
+function loadProducts() {
+  products.sort(function (a, b) { return a.value - b.value; });
+  document.getElementById("productList").innerHTML = "";
+
+  products.forEach(product => {
+    document.getElementById("productList").innerHTML += `
+      <tr>
+      <td>${product.name}</td>
+      <td>${product.value}</td>
+      </tr>
+    `
+  });
+}
+
+function cleanInputs() {
+  document.getElementById("nameInput").value = "";
+  document.getElementById("descriptionInput").value = "";
+  document.getElementById("valueInput").value = "";
+  document.getElementById("availabilityInput").value = "Sim";
+}
+
 function openModal() {
   document.getElementById("registerModal").classList.add("is-active")
 }
